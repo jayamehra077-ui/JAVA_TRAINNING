16be0e5328c3a7c100c35d01404a40488e837f1e
jayamehra077-ui <jayamehra077@gmail.com>
2025-11-04 13:07:16 +0530

    CODE

---
+++ lec2/src/string.java
@@ -0,0 +1,15 @@
+
+        public class string {
+            public static void main(String[] args)
+            {
+                String str="JAYA";
+                for(int i=0;i<str.length();i++)
+                {
+                    System.out.println(str.concat("MEHRA"));
+                    System.out.println(str.charAt(3));
+                }
+            }
+        }
+
+
+
