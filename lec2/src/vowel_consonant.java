a94c784935e29efcbbc48c8f749a625bc4028456
jayamehra077-ui <jayamehra077@gmail.com>
2025-11-07 13:00:31 +0530

    JAVA CODE

--- lec2/src/vowel_consonant.java
+++ lec2/src/vowel_consonant.java
@@ -1,4 +1,15 @@
 import java.util.Scanner;
 public class vowel_consonant {
+ class main {
+     public static void main(string[] args){
+         string str="hello";//'h','e','l','l','o';
+         int v=0;
+         for(char ch:str.toCharArray()){
+             if("aeuio",str.index0f(ch)!=-1){
+                 v++;
+             }
+         }
+     System.out.println(v);
+ }
 
 }
