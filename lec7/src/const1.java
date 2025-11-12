4e01e3f7d3d5536dfd5fb9d4970066122db4a36b
jayamehra077-ui <jayamehra077@gmail.com>
2025-11-12 13:23:51 +0530

    CODE

---
+++ lec7/src/const1.java
@@ -0,0 +1,15 @@
+public class const1 {
+    public static void main(String[] args) {
+        car obj=new car(5);
+        //obj.a;
+        System.out.println(obj.a);
+    }
+}
+    class car{
+        int a;
+        car (int a){
+            //a=b;
+            //a=a;
+            this.a=a;
+        }
+    }
