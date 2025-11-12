f056422571550ae5f70e838533257f7bab03b4e4
jayamehra077-ui <jayamehra077@gmail.com>
2025-11-12 12:43:26 +0530

    CODE

---
+++ lec7/src/constructor.java
@@ -0,0 +1,13 @@
+public class constructor {
+    public static void main(String[] args)
+    {
+        car obj=new car();
+    }
+}
+class car
+{
+    car()
+    {
+        System.out.println("HELLOW I AM JAYA");
+    }
+}
