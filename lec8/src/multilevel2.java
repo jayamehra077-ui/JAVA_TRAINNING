ac705d3d68c50769e4c43b477655b9c8533d0b43
jayamehra077-ui <jayamehra077@gmail.com>
2025-11-14 15:13:02 +0530

    CODE

---
+++ lec8/src/multilevel2.java
@@ -0,0 +1,24 @@
+public class multilevel2 {
+    public static void main(String[]args){
+        GamingLaptop g= new GamingLaptop();
+        g.PowerOn();
+        g.Boot();
+        g.startGame();
+    }
+}
+
+    class Device{
+        void PowerOn(){
+            System.out.println("Device is powered on");
+        }
+    }
+    class Laptop extends Device{
+        void Boot(){
+            System.out.println("Laptop is booting");
+        }
+    }
+    class GamingLaptop extends Laptop{
+        void startGame(){
+            System.out.println("Game is starting on laptop");
+        }
+    }
