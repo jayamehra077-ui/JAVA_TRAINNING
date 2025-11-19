fd3db4fbb65cf4cd971a390097798f1a2a815a24
jayamehra077-ui <jayamehra077@gmail.com>
2025-11-19 14:16:12 +0530

    CODE

--- lec11/src/abstraction.java
+++ lec11/src/abstraction.java
@@ -2,11 +2,19 @@ abstract class hello
 {
     abstract void intro();//abstract ka object nhi banta
     //abstract method ka override karna compulsory h
+    void about()
+    {
+        System.out.println("from NAVODAYA!!!!!!!!");
+    }
 }
 class hey extends hello{
     void intro()
     {
       System.out.println("hey everyone!!!!!!!!!\n myself jaya mehra");
+    }
+    void about()
+    {
+        System.out.println("from NAVODAYA!!!!!!!!");
     }
 }
 public class abstraction {
@@ -15,6 +23,8 @@ public class abstraction {
     {
       hey h1=new hey();
       h1.intro();
+      hello h2=new hey();
+      h2.about();
     }
 }
 
