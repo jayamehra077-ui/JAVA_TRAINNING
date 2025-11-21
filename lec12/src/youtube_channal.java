d3954b5de19a5d79341b380df33ec09535d49ecc
jayamehra077-ui <jayamehra077@gmail.com>
2025-11-21 14:14:43 +0530

    CODE

---
+++ lec12/src/youtube_channal.java
@@ -0,0 +1,34 @@
+public class youtube_channal {
+    public static void main(String[] args)
+    {
+        youtube y1=new youtube();
+        y1.get_subscriber();
+        y1.set_subscriber(300000);
+    }
+}
+class youtube
+{
+   public String user_id="sovereign_girl321";
+   public int total_subscriber=12000;
+    public void get_subscriber()
+    {
+        System.out.println("channal name="+user_id);
+
+System.out.print("total subscriber="+total_subscriber);
+        System.out.print("\n");
+    }
+
+    public void set_subscriber(int count)
+    {
+        if(count<total_subscriber)
+        {
+            total_subscriber-=count;
+            System.out.println("-!!!!!!!-subscriber km ho gye h,count="+count);
+        }
+        else
+        {
+            total_subscriber+=count;
+            System.out.println("-!!!!!!!-number of subscriber increase  ho gye h\ncount="+count);
+        }
+    }
+}
