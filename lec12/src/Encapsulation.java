92fb3f7d9c02f3cee1dd9bb3ed02b04c4b44c91e
jayamehra077-ui <jayamehra077@gmail.com>
2025-11-21 13:46:27 +0530

    CODE

---
+++ lec12/src/Encapsulation.java
@@ -0,0 +1,34 @@
+public class Encapsulation {
+    //SINGLE UNIT ME DATA KO BIND KARNA HI ENCAPSULATION KEHLATA H
+    public static void main(String[] args)
+    {
+        atm s1=new atm();
+        s1.getBalance();
+        s1.setBalance(200);
+    }
+}
+class atm
+{
+    private int totalBalance=100000;
+    public void getData()
+    {
+
+    }
+    public void getBalance()
+    {
+        System.out.println(totalBalance);
+    }
+    public void setBalance(int amount)
+    {
+        if(amount>0&&totalBalance>=amount)
+        {
+            totalBalance-=amount;
+            System.out.print("after withdraw amount="+totalBalance);
+        }
+       else
+        {
+            totalBalance+=amount;
+            System.out.print("after deposit="+totalBalance);
+        }
+    }
+}
