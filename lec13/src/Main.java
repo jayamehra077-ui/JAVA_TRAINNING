4927e27a099b324293c484c87e68acc80acf4937
jayamehra077-ui <jayamehra077@gmail.com>
2025-11-24 15:15:55 +0530

    PACAGES

---
+++ lec13/src/Main.java
@@ -0,0 +1,8 @@
+import p1.classA;
+    public static void main(String[] args)
+    {
+       // import p1.classA;
+        classA s1=new classA();
+        s1.show();
+    }
+
---
+++ lec13/src/p1/classA.java
@@ -0,0 +1,8 @@
+package p1;
+
+public class classA {
+    public void show()
+    {
+        System.out.println("----HEYYY-----");
+    }
+}
