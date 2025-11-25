cec210204cec919cbeb5e1862913112e9b8afc1e
jayamehra077-ui <jayamehra077@gmail.com>
2025-11-25 15:25:32 +0530

    ARRAY_LIST

---
+++ lec14/src/merge_array_list.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+public class merge_array_list {
+    public static void main(String[] args)
+    {
+        ArrayList<Integer> list1=new ArrayList<>();
+        ArrayList<Integer> list2=new ArrayList<>();
+        list1.add(0,12);
+        list1.add(1,2);
+        list1.add(2,24);
+        list2.add(0,25);
+        list2.add(1,13);
+        list2.add(2,19);
+        System.out.println("-----FIRST ARRAY_LIST---- ");
+        System.out.println(list1);
+        System.out.println("----SECOND ARRAY_LIST---");
+        System.out.println(list2);
+
+
+    }
+}
