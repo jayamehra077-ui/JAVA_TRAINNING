7517dd30b0e360268112a58ee5b7e0a2dffd46f4
jayamehra077-ui <jayamehra077@gmail.com>
2025-11-28 13:43:16 +0530

    TREESET_METHOD

---
+++ lec17/src/tree_set.java
@@ -0,0 +1,17 @@
+import java.util.LinkedHashSet;
+import java.util.*;
+public class tree_set {
+    //import java.util.*;
+   // public class linked_has_set {
+        public static void main(String[] args)
+        {
+            TreeSet<Integer> set=new TreeSet<>();
+            set.add(2);
+            set.add(142);
+            set.add(512);
+            set.add(912);
+            System.out.println(set);
+        }
+    }
+
+
