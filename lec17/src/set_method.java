75e634d6bed8cb9f44c4915e2227c693c55e291f
jayamehra077-ui <jayamehra077@gmail.com>
2025-11-28 13:01:48 +0530

    SET_METHOD

---
+++ lec17/src/set_method.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class set_method {
+    public static void main(String[] args)
+    {
+        HashSet<Integer> set=new HashSet<>();//insertion order maintain nohi hota
+        set.add(10);
+        set.add(20);
+        set.add(13);
+        set.add(50);
+        System.out.println(set);
+    }
+}
