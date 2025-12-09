aea9468d80fc0f50ee3c8980fcda7f915e58f018
jayamehra077-ui <jayamehra077@gmail.com>
2025-12-09 16:13:30 +0530

    EXCEPTION HANDLING

--- lec19/src/use_of_throws.java
+++ lec19/src/use_of_throws.java
@@ -1,3 +1,19 @@
 public class use_of_throws {
+    public static void main(String[] args)
+    {
+        try {
+            riskyMethod();
+        } catch (Exception e) {
+           // throw new RuntimeException(e);
+            System.out.println("main me handle hua:"+e);
+        }
+        System.out.println("program continue:...");
+    }
+    static void riskyMethod() throws ArithmeticException
+    {
+        System.out.println("riskyMethod start..");
+        int result=10/0;
+        System.out.println("result:"+result);
+    }
 
 }
