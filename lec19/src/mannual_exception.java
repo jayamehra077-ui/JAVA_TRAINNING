b8e4438e5cfead6ed7a6dfb86d911fcdddc1727b
jayamehra077-ui <jayamehra077@gmail.com>
2025-12-09 15:46:36 +0530

    EXCEPTION HANDLING

--- lec19/src/mannual_exception.java
+++ lec19/src/mannual_exception.java
@@ -1,3 +1,45 @@
-public class mannual_exception {
+/*public class mannual_exception {
+    public static void main(String[] args)
+    {
+      try {
+          voteAge(30);
+      } catch (Exception e) {
+         // throw new RuntimeException(e);
+          System.out.println(e);
+      }
+        try {
+            voteAge(15);
+        } catch (Exception e) {
+            // throw new RuntimeException(e);
+            System.out.println(e);
+        }
+    }
+
+}*/
+static void voteAge(int age)
+{
+    System.out.println("---CHECK FOR VOTE---");
+    if(age<18)
+    {
+        throw new IllegalArgumentException("aap vote nahi de sakte hai....");
+    }
+}
+
+void main() {
+   // public static void main(String[] args)
+    {
+        try {
+            voteAge(30);
+        } catch (Exception e) {
+            // throw new RuntimeException(e);
+            System.out.println(e);
+        }
+        try {
+            voteAge(15);
+        } catch (Exception e) {
+            // throw new RuntimeException(e);
+            System.out.println(e);
+        }
+    }
 
 }
