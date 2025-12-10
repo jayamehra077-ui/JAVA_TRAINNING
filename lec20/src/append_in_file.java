570966a11a38f72a4f86ef8bff6b116e0eeab5b1
jayamehra077-ui <jayamehra077@gmail.com>
2025-12-10 15:36:10 +0530

    FILE SYSTEM

---
+++ lec20/src/append_in_file.java
@@ -0,0 +1,38 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class append_in_file {
+    void main(){
+        // public static void main(String[] args)
+        {
+            create_data();
+            try {
+                File file=new File("index.html");
+                if(file.createNewFile())
+                {
+                    System.out.println("---done!! file created---");
+                }
+                else {
+                    System.out.println("---sorry!! file of this name is already created---");
+                }
+            } catch (IOException e) {
+                //  throw new RuntimeException(e);
+                System.out.println(e);
+            }
+        }
+    }
+    static void create_data()
+    {
+        try {
+            FileWriter writer=new FileWriter("index.html",true);
+           // writer.write("---heyyy everyone!!!!\n -----new data is inserted to the file----");
+            writer.write("ab aap pehle wala  data v dekh skte h ----");
+            writer.close();
+        } catch (Exception e) {
+            // throw new RuntimeException(e);
+            System.out.println(e);
+        }
+    }
+
+}
