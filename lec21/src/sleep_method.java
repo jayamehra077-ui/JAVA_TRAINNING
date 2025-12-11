9eb20c67c81da2ec8b914a6f917071d4e7d26b20
jayamehra077-ui <jayamehra077@gmail.com>
2025-12-11 11:16:05 +0530

    MULTI_THREADING

---
+++ lec21/src/sleep_method.java
@@ -0,0 +1,39 @@
+public class sleep_method {
+    public static void main(String[] args)
+    {
+        System.out.println(Thread.currentThread().getName());
+        My_thread2 t1=new My_thread2();
+        t1.start();
+        try {
+            t1.join();
+
+        } catch (Exception e) {
+            // throw new RuntimeException(e);
+            System.out.println(e);
+        }
+        for(int i=0;i<5;i++)
+        {
+            System.out.println(i+"main");
+        }
+    }
+}
+class My_thread2 extends Thread
+{
+    public void  run()
+    {
+        System.out.println(Thread.currentThread().getName());
+        System.out.println("---hiiiii----");
+        for(int i=0;i<5;i++)
+        {
+            System.out.println(i+"t1");
+            try {
+                Thread.sleep(500);
+
+            } catch (Exception e) {
+//throw new RuntimeException(e);
+                System.out.println(e);
+            }
+        }
+    }
+}
+
