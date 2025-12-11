94a2bf67b9385d2d56a4ca49e46b57bef99d2188
jayamehra077-ui <jayamehra077@gmail.com>
2025-12-11 11:37:51 +0530

    MULTI_THREADING

---
+++ lec21/src/parallel_execution.java
@@ -0,0 +1,42 @@
+public class parallel_execution {
+    public static void main(String[] args)
+    {
+        System.out.println(Thread.currentThread().getName());
+        My_thread3 t1=new My_thread3();
+        t1.start();
+        /*try {
+            t1.join();
+
+        } catch (Exception e) {
+            // throw new RuntimeException(e);
+            System.out.println(e);
+        }*/
+        for(int i=0;i<10;i++)
+        {
+            System.out.println("timer"+" "+i);}
+
+
+    }
+}
+class My_thread3 extends Thread
+{
+    public void  run()
+    {
+        System.out.println(Thread.currentThread().getName());
+        System.out.println("---hiiiii----");
+        for(int i=0;i<10;i++)
+        {
+           // System.out.println(i+"t1");
+            System.out.println("downloading file....."+(i*10)+"%");
+            try {
+                Thread.sleep(1000);
+
+            } catch (Exception e) {
+//throw new RuntimeException(e);
+                System.out.println(e);
+            }
+        }
+    }
+}
+
+
