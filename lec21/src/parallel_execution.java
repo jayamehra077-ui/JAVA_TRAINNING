51152eb1e572703784538ef543067357f7e2fdb6
jayamehra077-ui <jayamehra077@gmail.com>
2025-12-11 11:46:27 +0530

    MULTI_THREADING

--- lec21/src/parallel_execution.java
+++ lec21/src/parallel_execution.java
@@ -1,8 +1,7 @@
 public class parallel_execution {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         System.out.println(Thread.currentThread().getName());
-        My_thread3 t1=new My_thread3();
+        My_thread3 t1 = new My_thread3();
         t1.start();
         /*try {
             t1.join();
@@ -11,29 +10,31 @@ public class parallel_execution {
             // throw new RuntimeException(e);
             System.out.println(e);
         }*/
-        for(int i=0;i<10;i++)
-        {
-            System.out.println("timer"+" "+i);}
+        for (int i = 0; i < 10; i++) {
+            System.out.println("timer" + " " + i);
+        }
+        try {
+            Thread.sleep(4000);
 
+        } catch (Exception e) {
 
+        }
     }
-}
-class My_thread3 extends Thread
-{
-    public void  run()
-    {
-        System.out.println(Thread.currentThread().getName());
-        System.out.println("---hiiiii----");
-        for(int i=0;i<10;i++)
-        {
-           // System.out.println(i+"t1");
-            System.out.println("downloading file....."+(i*20)+"%");
-            try {
-                Thread.sleep(5000);
 
-            } catch (Exception e) {
+   static class My_thread3 extends Thread {
+        public void run() {
+            System.out.println(Thread.currentThread().getName());
+            System.out.println("---hiiiii----");
+            for (int i = 0; i < 10; i++) {
+                // System.out.println(i+"t1");
+                System.out.println("downloading file....." + (i * 20) + "%");
+                try {
+                    Thread.sleep(5000);
+
+                } catch (Exception e) {
 //throw new RuntimeException(e);
-                System.out.println(e);
+                    System.out.println(e);
+                }
             }
         }
     }
