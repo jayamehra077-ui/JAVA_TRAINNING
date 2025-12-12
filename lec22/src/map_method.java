25c764be4fb0ea4b8a4cd9871a101875ca73e954
jayamehra077-ui <jayamehra077@gmail.com>
2025-12-12 13:48:55 +0530

    JAVA

--- lec22/src/map_method.java
+++ lec22/src/map_method.java
@@ -1,2 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class map_method {
+    public static void main(String[] args)
+    {
+        ArrayList<Integer> list=new ArrayList<>();
+        list.add(20);
+        list.add(90);
+        list.add(27);
+        list.add(23);
+        System.out.print("-----list-----\n"+""+list);
+        System.out.println("\n");
+        List<Integer> map=list.stream().map(n->n*2).toList();
+        System.out.print("----elements of list after multiply by 2-----\n"+""+map);
+    }
 }
+
+
