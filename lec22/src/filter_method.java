c4a8db9ac03d7348351db0a811811fd735743745
jayamehra077-ui <jayamehra077@gmail.com>
2025-12-12 13:56:36 +0530

    JAVA

--- lec22/src/filter_method.java
+++ lec22/src/filter_method.java
@@ -11,7 +11,7 @@ public class filter_method {
         list.add(23);
         System.out.print("-----list-----\n"+""+list);
         System.out.println("\n");
-        List<Integer> result=list.stream().filter(n->n>23).toList();
+        List<Integer> result=list.stream().filter(n->n>20).toList();
         System.out.print("-----elements of list greater less than 90-----\n"+""+result);
     }
 }
