19a61cc8f808a6d1f8444d554ac2670fe632035b
jayamehra077-ui <jayamehra077@gmail.com>
2025-12-12 14:04:05 +0530

    JAVA

--- lec22/src/sorted_method.java
+++ lec22/src/sorted_method.java
@@ -1,2 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class sorted_method {
+    public static void main(String[] args)
+    {
+        ArrayList<Integer> list=new ArrayList<>();
+        list.add(20);
+        list.add(90);
+        list.add(27);
+        list.add(23);
+        System.out.print("-----list-----\n"+""+list);
+        System.out.println("\n");
+        List<Integer> sort=list.stream().sorted((a,b)->a-b).toList();
+        System.out.print("----ascending order-----\n"+""+sort);
+    }
 }
+
+
