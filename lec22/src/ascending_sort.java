ed718f0725d7d504cb6566c65402c7dd2bc314a1
jayamehra077-ui <jayamehra077@gmail.com>
2025-12-12 14:08:28 +0530

    JAVA

--- lec22/src/ascending_sort.java
+++ lec22/src/ascending_sort.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class sorted_method {
+public class ascending_sort {
     public static void main(String[] args)
     {
         ArrayList<Integer> list=new ArrayList<>();
