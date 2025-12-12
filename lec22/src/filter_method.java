fa1dad9471a0a6c06e9d4ae1df90e1c4be55bdf5
jayamehra077-ui <jayamehra077@gmail.com>
2025-12-12 13:38:31 +0530

    JAVA

---
+++ lec22/src/filter_method.java
@@ -0,0 +1,2 @@
+public class filter_method {
+}
---
+++ lec22/src/reduce_method.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class reduce_method {
+    public static void main(String[] args)
+    {
+        ArrayList<Integer>list=new ArrayList<>();
+        list.add(20);
+        list.add(90);
+        list.add(27);
+        list.add(23);
+        System.out.print("-----list-----\n"+""+list);
+        System.out.println("\n");
+        List<Integer> result=list.stream().filter(n->n<90).toList();
+        System.out.print("-----elements of list greater less than 90-----\n"+""+result);
+    }
+}
