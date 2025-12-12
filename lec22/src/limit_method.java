1c8f70502d8a0ea2121d4473a1d83a88737dd6ab
jayamehra077-ui <jayamehra077@gmail.com>
2025-12-12 14:07:39 +0530

    JAVA

---
+++ lec22/src/limit_method.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class limit_method {
+    public static void main(String[] args)
+    {
+        ArrayList<Integer> list=new ArrayList<>();
+        list.add(20);
+        list.add(90);
+        list.add(27);
+        list.add(23);
+        System.out.print("-----list-----\n"+""+list);
+        System.out.println("\n");
+        List<Integer> limit=list.stream().limit(2).toList();
+        System.out.print("----elements of list after limiting 2 elements-----\n"+""+limit);
+    }
+}
+
+
