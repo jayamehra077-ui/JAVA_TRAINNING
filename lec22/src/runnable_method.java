ae54a2e4e2509193516dbf2531000ad081b5593a
jayamehra077-ui <jayamehra077@gmail.com>
2025-12-12 13:08:23 +0530

    MULTI_THREADING

---
+++ lec22/src/runnable_method.java
@@ -0,0 +1,26 @@
+public class runnable_method {
+    public static void main(String[] args)
+    {
+        my_thread task=new my_thread();
+        Thread t1=new Thread();
+        Thread t2=new Thread();
+        t1.start();
+        t2.start();
+    }
+}
+class my_thread implements Runnable
+{
+    @Override
+    public void run() {
+        for(int i=0;i<10;i++)
+        {System.out.println(i);
+            try {
+                Thread.sleep(200);
+            } catch (Exception e) {
+                //throw new RuntimeException(e);
+                System.out.println(e);
+            }
+            }
+       // System.out.println("---hey everyone--");
+    }
+}
