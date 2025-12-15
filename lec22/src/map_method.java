4f704624f2a2a21754688dc88dbfcbf43a0b0f64
jayamehra077-ui <jayamehra077@gmail.com>
2025-12-15 22:41:42 +0530

    code

--- lec22/src/map_method.java
+++ lec22/src/map_method.java
@@ -5,10 +5,10 @@ public class map_method {
     public static void main(String[] args)
     {
         ArrayList<Integer> list=new ArrayList<>();
-        list.add(20);
-        list.add(90);
-        list.add(27);
-        list.add(23);
+        list.add(200);
+        list.add(900);
+        list.add(207);
+        list.add(203);
         System.out.print("-----list-----\n"+""+list);
         System.out.println("\n");
         List<Integer> map=list.stream().map(n->n*100).toList();
