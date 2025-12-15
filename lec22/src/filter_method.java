4d935e0bc68905e61b59d2e95d67e498d8ff9776
jayamehra077-ui <jayamehra077@gmail.com>
2025-12-15 22:45:25 +0530

    code

--- lec22/src/filter_method.java
+++ lec22/src/filter_method.java
@@ -8,7 +8,11 @@ public class filter_method {
         list.add(20);
         list.add(90);
         list.add(27);
+        list.add(29);
+        list.add(89);
+        list.add(34);
         list.add(23);
+        list.add(45);
         System.out.print("-----list-----\n"+""+list);
         System.out.println("\n");
         List<Integer> result=list.stream().filter(n->n>20).toList();
