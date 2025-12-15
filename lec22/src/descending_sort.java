2d74017c7ee36d484ea7695efde9c189016bf7fa
jayamehra077-ui <jayamehra077@gmail.com>
2025-12-15 22:44:10 +0530

    code

--- lec22/src/descending_sort.java
+++ lec22/src/descending_sort.java
@@ -8,7 +8,10 @@ public class descending_sort {
         list.add(20);
         list.add(90);
         list.add(27);
-        list.add(23);
+        list.add(29);
+        list.add(24);
+        list.add(53);
+        list.add(83);
         System.out.print("-----list elements are-----\n"+""+list);
         System.out.println("\n");
         List<Integer> sort=list.stream().sorted((a, b)->b-a).toList();
