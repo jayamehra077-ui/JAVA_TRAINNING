73a21d09326d122b30212fc5f43459889fe9ebd9
jayamehra077-ui <jayamehra077@gmail.com>
2025-12-15 22:47:36 +0530

    code

--- lec22/src/limit_method.java
+++ lec22/src/limit_method.java
@@ -8,10 +8,13 @@ public class limit_method {
         list.add(20);
         list.add(90);
         list.add(27);
-        list.add(23);
+        list.add(33);
+        list.add(53);
+        list.add(73);
+        list.add(63);
         System.out.print("-----list-----\n"+""+list);
         System.out.println("\n");
-        List<Integer> limit=list.stream().limit(3).toList();
+        List<Integer> limit=list.stream().limit().toList();
         System.out.print("----elements of list after limiting 3 elements-----\n"+""+limit);
     }
 }
