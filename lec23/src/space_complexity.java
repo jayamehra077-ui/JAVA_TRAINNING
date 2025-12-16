115d66a3d3573f27712048e8fdd3dbafbf0c71f2
jayamehra077-ui <jayamehra077@gmail.com>
2025-12-16 15:39:29 +0530

    JAVA

--- lec23/src/space_complexity.java
+++ lec23/src/space_complexity.java
@@ -1,3 +1,25 @@
-public class space_complexity {
+import java.util.HashMap;
 
+public class space_complexity {
+    public static void main(String[] args)
+    {
+       // int majority_element(int[])
+
+
+                  //  HashMap < Integer, Integer > map = new HashMap<>();
+        }
+    }
+
+public int majority_element(int[] nums)
+{
+    HashMap < Integer, Integer > map = new HashMap<>();
+    for(int i:nums)
+    {
+        map.put(i,map.getOrDefault(i,0)+1);
+        if(map.get(i)>nums.length/2)
+        {
+            return 1;
+        }
+    }
+    return -1;
 }
