b5b60cb52082b8e5b0526c9b5b53de79c1e7621f
jayamehra077-ui <jayamehra077@gmail.com>
2025-12-16 15:28:27 +0530

    JAVA

--- lec22/src/skip_method.java
+++ lec22/src/skip_method.java
@@ -11,8 +11,8 @@ public class skip_method {
         list.add(23);
         System.out.print("-----list-----\n"+""+list);
         System.out.println("\n");
-        List<Integer> skip=list.stream().skip(4).toList();
-        System.out.print("----elements of list after skiping 2 elements-----\n"+""+skip);
+        List<Integer> skip=list.stream().skip(1).toList();
+        System.out.print("----elements of list after skiping 1 elements-----\n"+""+skip);
     }
 }
 
