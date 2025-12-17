a3e134aa5ca6f18d012b5c74ffbd5d53033a7a8d
jayamehra077-ui <jayamehra077@gmail.com>
2025-12-17 16:14:24 +0530

    JAVA

---
+++ lec24/src/DNF.java
@@ -0,0 +1,30 @@
+public class DNF {
+    public static void main(String[] args)
+    {
+        int arr[]={1,2,1,1,0,0,2,0};
+        int low=0;
+        int mid=0;
+        int h=arr.length-1;
+        while(mid<=h)
+        {
+            if(arr[mid]==0)
+            {
+               // swap(arr,low,mid);
+                low++;
+                mid++;
+            }
+            if(arr[mid]==1)
+            {
+
+
+                mid++;
+            }
+            if(arr[mid]==2)
+            {
+               // swap(arr,mid,h);
+
+                mid--;
+            }
+        }
+    }
+}
