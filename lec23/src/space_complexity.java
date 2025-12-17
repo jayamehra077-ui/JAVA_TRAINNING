7b4fa5e8f18fd2810294913945f43c22c7eb08b8
jayamehra077-ui <jayamehra077@gmail.com>
2025-12-17 15:26:39 +0530

    JAVA

--- lec23/src/space_complexity.java
+++ lec23/src/space_complexity.java
@@ -1,6 +1,6 @@
 import java.util.HashMap;
 
-public class space_complexity {
+/*public class space_complexity {
     public static void main(String[] args)
     {
 
@@ -8,18 +8,19 @@ public class space_complexity {
 
                   //  HashMap < Integer, Integer > map = new HashMap<>();
         }
-    }
+    }*/
+//int main() {
+    //majority_element ( 1,2,3,4,2,2,3,3,3,4,4);
 
-public int majority_element(int[] nums)
-{
-    HashMap < Integer, Integer > map = new HashMap<>();
-    for(int i:nums)
+
+    public int majority_element ( int[] nums)
     {
-        map.put(i,map.getOrDefault(i,0)+1);
-        if(map.get(i)>nums.length/2)
-        {
-            return 1;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i : nums) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+            if (map.get(i) > nums.length / 2) {
+                return 1;
+            }
         }
+        return -1;
     }
-    return -1;
-}
---
+++ lec24/src/sort_numbers.java
@@ -0,0 +1,55 @@
+public class sort_numbers {
+    public static void main(String[] args)
+    {
+       // sort_color();
+        int[] colors = {2, 0, 2, 1, 1, 0};
+        System.out.println("Original array: " + java.util.Arrays.toString(colors));
+
+        solution sol = new solution();
+        sol.sort_color(colors);
+
+        System.out.println("Sorted array:   " + java.util.Arrays.toString(colors));
+    }
+
+}
+class solution
+{
+    public void sort_color(int[] nums)
+    {
+        int c1=0;
+        int c2=0;
+        int c3=0;
+        for(int i:nums)
+        {
+            if(i==0)
+            {
+                c1++;
+            }
+            else {
+                if(i==0)
+                {
+                   c2++;
+                }
+                else {
+                    c3++;
+                }
+            }
+        }
+        int idx=0;
+        for(int i=0;i<c1;i++)
+        {
+            nums[idx]=1;
+            idx++;
+        }
+        for(int i=0;i<c2;i++)
+        {
+            nums[idx]=1;
+            idx++;
+        }
+        for(int i=0;i<c3;i++)
+        {
+            nums[idx]=1;
+            idx++;
+        }
+    }
+}
