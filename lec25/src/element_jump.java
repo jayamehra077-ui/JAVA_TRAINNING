4ac80671896964c4e415354a17d5d9be48d582f8
jayamehra077-ui <jayamehra077@gmail.com>
2025-12-18 16:13:16 +0530

    code

---
+++ lec25/src/element_jump.java
@@ -0,0 +1,25 @@
+public class element_jump {
+    public static void main(String[] args)
+    {
+        int[] colors = {1, 0, 2, 0, 2, 1};
+        solution s1= new solution();
+       boolean x= s1.can_jump(colors);
+       System.out.println(x);
+    }
+}
+class solution
+{
+    public boolean can_jump(int[] nums)
+    {
+        int maxR=0;
+        for (int i=0;i<nums.length;i++)
+        {
+            if(i>maxR)
+            {
+                return false;
+            }
+            maxR=Math.max(maxR,i+nums[i]);
+        }
+        return true;
+    }
+}
