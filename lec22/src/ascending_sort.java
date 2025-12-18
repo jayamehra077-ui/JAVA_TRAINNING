a399b2f4fd6264cee710778df1cc6cf4506dd735
jayamehra077-ui <jayamehra077@gmail.com>
2025-12-18 15:53:06 +0530

    JAVA

--- lec22/src/ascending_sort.java
+++ lec22/src/ascending_sort.java
@@ -6,10 +6,10 @@ public class ascending_sort {
     {
         ArrayList<Integer> list=new ArrayList<>();
         list.add(20);
-        list.add(90);
+        list.add(9);
         list.add(27);
         list.add(93);
-        list.add(28);
+        list.add(2);
         list.add(21);
         list.add(73);
         System.out.print("-----liste elements are-----\n"+""+list);
