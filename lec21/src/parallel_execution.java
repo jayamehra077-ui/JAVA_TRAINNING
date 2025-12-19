5d62834edbbd67e2a4b2d94f919ba12977c4adfa
jayamehra077-ui <jayamehra077@gmail.com>
2025-12-19 20:51:33 +0530

    code

--- lec21/src/parallel_execution.java
+++ lec21/src/parallel_execution.java
@@ -14,7 +14,7 @@ public class parallel_execution {
             System.out.println("timer" + " " + i);
         }
         try {
-            Thread.sleep(400);
+            Thread.sleep(100);
 
         } catch (Exception e) {
 
