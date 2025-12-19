45f33695d713ffb81195c35dca9f1aef3f9bda1d
jayamehra077-ui <jayamehra077@gmail.com>
2025-12-19 20:50:14 +0530

    code

--- lec20/src/append_in_file.java
+++ lec20/src/append_in_file.java
@@ -14,7 +14,7 @@ public class append_in_file {
                     System.out.println("---done!!\n file created---");
                 }
                 else {
-                    System.out.println("---sorry!! file of this name is already created---");
+                    System.out.println("---sorry guyss!!\n file of this name is already created---");
                 }
             } catch (IOException e) {
                 //  throw new RuntimeException(e);
