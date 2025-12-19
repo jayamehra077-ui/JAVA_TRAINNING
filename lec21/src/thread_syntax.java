ecafe1eb6cab90dec174c063832da4a5849fe3ff
jayamehra077-ui <jayamehra077@gmail.com>
2025-12-19 20:46:17 +0530

    code

--- lec21/src/thread_syntax.java
+++ lec21/src/thread_syntax.java
@@ -15,7 +15,7 @@ class My_thread extends Thread
     public void  run()
     {
        System.out.println(Thread.currentThread().getName());
-        System.out.println("---hiiiii----");
+        System.out.println("--hiiiii--");
         for(int i=0;i<5;i++)
         {
             System.out.println(i+"t1");
