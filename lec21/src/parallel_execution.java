3e83eab4265366ce3ca62ae9c245e724089b741e
jayamehra077-ui <jayamehra077@gmail.com>
2025-12-22 14:59:55 +0530

    code

--- lec21/src/parallel_execution.java
+++ lec21/src/parallel_execution.java
@@ -27,7 +27,7 @@ public class parallel_execution {
             System.out.println("---hiiiii----");
             for (int i = 0; i < 10; i++) {
                 // System.out.println(i+"t1");
-                System.out.println("downloading file....." + (i * 10) + "%");
+                System.out.println("downloading file....." + (i * 20) + "%");
                 try {
                     Thread.sleep(1000);
 
