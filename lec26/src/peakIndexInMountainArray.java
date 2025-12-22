944890f1579151a8210895a50fb92808a2b89782
jayamehra077-ui <jayamehra077@gmail.com>
2025-12-22 15:32:57 +0530

    code

--- lec26/src/peakIndexInMountainArray.java
+++ lec26/src/peakIndexInMountainArray.java
@@ -1,6 +1,31 @@
 public class peakIndexInMountainArray {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+        int[]  a={1,0,4,9,6,2};
+        solution s1=new solution();
+        s1.peak_index(a);
+
 
     }
+}
+class solution
+{
+    public int peak_index(int[] arr)
+    {
+
+
+    //int arr[] = {2, 0, 4, 6, 9};
+    int low = 0;
+    int height = arr.length;
+        while (low < height) {
+            int mid = (low + height) / 2;
+            if (arr[mid] < arr[mid + 1]) {
+                low = mid + 1;
+            } else {
+                height = mid;
+            }
+        }
+System.out.println(low);
+    return low;
+}
+
 }
