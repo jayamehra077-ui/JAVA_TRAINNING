dc52a8fd445faa99c0606c4c5d2983d43b445e9a
jayamehra077-ui <jayamehra077@gmail.com>
2025-12-25 14:43:44 +0530

    code

--- lec22/src/ascending_sort.java
+++ lec22/src/ascending_sort.java
@@ -7,11 +7,11 @@ public class ascending_sort {
         ArrayList<Integer> list=new ArrayList<>();
         list.add(0);
         list.add(90);
-        list.add(7);
+        list.add(71);
         list.add(93);
-        list.add(2);
+        list.add(21);
         list.add(10);
-        list.add(3);
+        list.add(31);
         System.out.print("-----liste elements are: -----\n"+""+list);
         System.out.println("\n");
         List<Integer> sort=list.stream().sorted((a,b)->a-b).toList();
