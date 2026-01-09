aa0ed1910699615785274c6b82ea54c6560bfd2f
jayamehra077-ui <jayamehra077@gmail.com>
2026-01-09 12:11:09 +0530

    code

--- DSA_SORTING/src/MERGE_SORT.java
+++ DSA_SORTING/src/MERGE_SORT.java
@@ -1,7 +1,7 @@
 public class MERGE_SORT {
     public static void main(String[] args)
     {
-        int[] arr={2,4,6,8,9,1};
+        int[] arr={2,4,6,8,9,1,3};
         merge_sort_array_list m1=new merge_sort_array_list();
         m1.sort(arr,0, arr.length);
     }
