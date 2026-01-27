a84e19bf80bf368b87a67f2cc099ca7565487aae
jayamehra077-ui <jayamehra077@gmail.com>
2026-01-27 11:26:59 +0530

    JAVA

--- lec26/src/peakIndexInMountainArray.java
+++ lec26/src/peakIndexInMountainArray.java
@@ -1,6 +1,6 @@
 public class peakIndexInMountainArray {
     public static void main(String[] args) {
-        int[]  a={1,0,4,7,8};
+        int[]  a={1,0,4,7,8,2};
         solution s1=new solution();
         s1.peak_index(a);
 
