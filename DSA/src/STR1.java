1a941f4136b8a2ce1f085def7af33e7b1bcf5fd2
jayamehra077-ui <jayamehra077@gmail.com>
2026-04-08 13:48:46 +0530

    CODE

---
+++ DSA/src/STR1.java
@@ -0,0 +1,18 @@
+public class STR1 {
+    public static void main(String[] args)
+    {
+        String str1="Hello";
+        String str2="Hiii";
+        System.out.println(str1);
+        System.out.println(str2);
+        System.out.println(str1.length());
+        System.out.println(str1.equals(str2));
+        System.out.println(str1.substring(0,3));
+        System.out.println(str1.replace("e","oo"));
+        System.out.println(str1.charAt(3));
+        System.out.println(str1.toLowerCase());
+        System.out.println(str1.toUpperCase());
+
+
+    }
+}
